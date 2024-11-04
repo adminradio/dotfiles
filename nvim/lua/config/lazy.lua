5e0d57d7f74df4a7666b79009ffe6f8a012c363e
Timm Wimmers <timm@wimmers.com>
2024-11-04 22:56:23 +0100

    Autoload and setup colorscheme

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -1,7 +1,6 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
   local out = vim.fn.system({
     "git",
     "clone",
@@ -58,3 +57,7 @@ require("lazy").setup({
     },
   },
 })
+
+-- FIX: Load catppuccin (this should be in `plugins/colorscheme.lua`, right? How?)
+require("catppuccin").setup()
+vim.cmd.colorscheme("catppuccin")
